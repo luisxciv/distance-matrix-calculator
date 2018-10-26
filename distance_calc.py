ace69357325efba10fd8108cd13f7ac431ba82a2
Luis Garcia Calzada <luisxciv@gmail.com>
2018-10-26 16:00:43 -0500

    indentation update

--- distance_calc.py
+++ distance_calc.py
@@ -3,7 +3,6 @@ import numpy as np
 import paramiko
 import pymysql.cursors
 from sshtunnel import SSHTunnelForwarder
-from paramiko import SSHClient
 from os.path import expanduser
 
 class Colour:
@@ -32,65 +31,65 @@ mypkey = paramiko.RSAKey.from_private_key_file(home + pkeyfilepath)
 
 ##SSHTUNNEL
 
-with SSHTunnelForwarder(
-        ('', 22),
-    ssh_username='',
-    ssh_pkey=mypkey,
-    remote_bind_address=('', 3306)) as tunnel:
-    rdsConn = pymysql.connect(host='127.0.0.1',
-                              db='',
-                              user='',
+#with SSHTunnelForwarder(
+#        ('', 22),
+#    ssh_username='',
+#    ssh_pkey=mypkey,
+#    remote_bind_address=('', 3306)) as tunnel:
+rdsConn = pymysql.connect(host='127.0.0.1',
+                              db='napify',
+                              user='root',
                               password='',
-                              port=tunnel.local_bind_port,
+                              port=3306,
                               charset='utf8mb4',
                               cursorclass=pymysql.cursors.DictCursor)
 
 
 #Initialize cursors
 
-    cursor1 = rdsConn.cursor()
-    cursor3 = rdsConn.cursor()
-    cursor4 = rdsConn.cursor()
+cursor1 = rdsConn.cursor()
+cursor3 = rdsConn.cursor()
+cursor4 = rdsConn.cursor()
 
-    sql = """select distinct mobile_user_id from score 
+sql = """select distinct mobile_user_id from score 
             where speed_range_id in (2, 9, 10, 11, 12, 13) and mobile_user_id > 516 
      order by mobile_user_id asc"""
-    distance_query = """SELECT created_date, latitude, longitude FROM score s where s.mobile_user_id = %(mobile_user_id)s and speed_range_id > 1 group by latitude, longitude order by id asc"""
-    insert_query = "Update mobile_user set avg_speed=%s, avg_distance=%s, avg_time=%s where mobile_user_id = %(mobile_user_id)s "
-    #col_names = ['created_date', 'latitude', 'longitude']
+distance_query = """SELECT created_date, latitude, longitude FROM score s where s.mobile_user_id = %(mobile_user_id)s and speed_range_id > 1 group by latitude, longitude order by id asc"""
+insert_query = "Update mobile_user set avg_speed=%s, avg_distance=%s, avg_time=%s where mobile_user_id = %(mobile_user_id)s "
+#col_names = ['created_date', 'latitude', 'longitude']
 
-    cursor1.execute(sql)
-    result = cursor1.fetchall()
+cursor1.execute(sql)
+result = cursor1.fetchall()
 
-    for row1 in result:
-        distance = cursor3.execute(distance_query, row1)
-        distance_result = cursor3.fetchall()
+for row1 in result:
+    distance = cursor3.execute(distance_query, row1)
+    distance_result = cursor3.fetchall()
 
-        df = pd.DataFrame(distance_result)
-        #merge our data
-        m = df.reset_index().merge(df.reset_index(), on='created_date')
-        m = m[m.index_x != m.index_y].drop(columns=['index_x', 'index_y'])
+    df = pd.DataFrame(distance_result)
+    #merge our data
+    m = df.reset_index().merge(df.reset_index(), on='created_date')
+    m = m[m.index_x != m.index_y].drop(columns=['index_x', 'index_y'])
 
-        #change our dataset from object type to float
-        m["longitude_x"] = m.longitude_x.astype(float)
-        m["longitude_y"] = m.longitude_y.astype(float)
-        m["latitude_x"] = m.latitude_x.astype(float)
-        m["latitude_y"] = m.latitude_y.astype(float)
+    #change our dataset from object type to float
+    m["longitude_x"] = m.longitude_x.astype(float)
+    m["longitude_y"] = m.longitude_y.astype(float)
+    m["latitude_x"] = m.latitude_x.astype(float)
+    m["latitude_y"] = m.latitude_y.astype(float)
 
         #Calculate Distance
-        m['Distance'] = haversine(m.longitude_x, m.latitude_x, m.longitude_y, m.latitude_y)
+    m['Distance'] = haversine(m.longitude_x, m.latitude_x, m.longitude_y, m.latitude_y)
 
-        #Count the number if ID's
-        count = df.created_date.count()
-        grouped = m.groupby('created_date').Distance.mean()
-        days = pd.to_datetime(m.iloc[:, 0]).dt.normalize().nunique()
-        total = sum(grouped)
+    #Count the number if ID's
+    count = df.created_date.count()
+    grouped = m.groupby('created_date').Distance.mean()
+    days = pd.to_datetime(m.iloc[:, 0]).dt.normalize().nunique()
+    total = sum(grouped)
 
-        id = int(row1["mobile_user_id"])
-        print("ID de usuario: " + Colour.RED + str(int(row1["mobile_user_id"])) + Colour.END)
-        print("Recorrido un total de distancia de" + Colour.RED + str(round(total, 1)) + Colour.END + " km")
+    id = int(row1["mobile_user_id"])
+    print("ID de usuario: " + Colour.RED + str(int(row1["mobile_user_id"])) + Colour.END)
+    print("Recorrido un total de distancia de" + Colour.RED + str(round(total, 1)) + Colour.END + " km")
 
-        print(Colour.RED + "=================================================" + Colour.END)
-        rdsConn.commit()
-    rdsConn.close()
+    print(Colour.RED + "=================================================" + Colour.END)
+    rdsConn.commit()
+rdsConn.close()
 
